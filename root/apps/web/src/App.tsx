cd32246858c3f888d60d983b213b714013645c95
SirDunners <neildunabie@hotmail.com>
2026-05-18 22:59:45 +0100

    Update kick-off point

--- root/apps/web/src/App.tsx
+++ root/apps/web/src/App.tsx
@@ -1,6 +1,21 @@
+import { useProcessStackState } from "../../state/useProcessStackState";
 
-import ProcessStackMVP from "./ProcessStackMVP";
+import PersonasTab from "../../tabs/PersonasTab";
+import ProcessesTab from "../../tabs/ProcessesTab";
+import DataModelsTab from "../../tabs/DataModelsTab";
+import SystemsTab from "../../tabs/SystemsTab";
+import IntegrationsTab from "../../tabs/IntegrationsTab";
+import ArchitectureTab from "../../tabs/ArchitectureTab";
 
 export default function App() {
-  return <ProcessStackMVP />;
+  const { tab } = useProcessStackState();
+
+  if (tab === "personas") return <PersonasTab />;
+  if (tab === "processes") return <ProcessesTab />;
+  if (tab === "dataModels") return <DataModelsTab />;
+  if (tab === "systems") return <SystemsTab />;
+  if (tab === "integrations") return <IntegrationsTab />;
+  if (tab === "architecture") return <ArchitectureTab />;
+
+  return <div>Welcome to ProcessStack</div>;
 }
